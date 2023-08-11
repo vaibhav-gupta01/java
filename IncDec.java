ec3062ea1490ac57346faf53256b9f8b4a3d1e69
vaibhav-gupta01 <132429205+vaibhav-gupta01@users.noreply.github.com>
2023-08-11 23:12:17 +0530

    Create IncDec.java

---
+++ IncDec.java
@@ -0,0 +1,16 @@
+class IncDec 
+{
+	public static void main(String[] args) 
+	{
+		//inc
+		int a=10;
+		System.out.println(a);    //10
+		System.out.println(a++);  //
+		System.out.println(++a);  //
+		System.out.println(a--);  //
+		System.out.println(--a);  //
+		  //10 10 12 12 10 
+		
+
+	}
+}
