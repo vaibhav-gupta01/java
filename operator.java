070b6a13bf7b15fba51dd284642ccfc6c0e8cad2
vaibhav-gupta01 <132429205+vaibhav-gupta01@users.noreply.github.com>
2023-08-11 23:10:30 +0530

    Create operator.java

---
+++ operator.java
@@ -0,0 +1,22 @@
+class Operator
+{
+	
+	public static void main(String[] args) 
+	{
+
+
+		int a=10;
+		int b=2;
+		//10+2=12
+		int res = a+b;
+		int min = a-b;
+		int div = a/b;
+		int mod = a%b;
+		System.out.println("a+b = "+ res);
+		System.out.println("a-b = "+ min);
+		System.out.println("a/b = "+ div);
+		System.out.println("a%b = "+ mod);
+		System.out.println("a+b = "+ a+b);
+		System.out.println(a+b+"= a+b");
+	}
+}
